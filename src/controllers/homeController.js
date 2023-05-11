23e798697f10b076a1b11e63e83fd280a1eff749
ntbchau <ntbchau2301@gmail.com>
2023-05-11 16:30:07 +0200

    Connect DB

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,16 @@
-let getHomePage = (req, res) => {
-    return res.render("homePage.ejs")
+const { cache } = require("ejs");
+
+import db from '../models/index';
+
+
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        return res.render('homePage.ejs', { data: JSON.stringify(data) })
+    } catch (e) {
+        console.log(e)
+    }
+
 }
 
 module.exports = {
