c8a498a88d3fdee14018874a3201fa44ec72c966
ntbchau <ntbchau2301@gmail.com>
2023-05-27 18:32:52 +0200

    get APIs 2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -24,6 +24,14 @@ let handleLogin = async (req, res) => {
 
 let handleGetAllUser = async (req, res) => {
     let id = req.body.id //all or id
+    if (!id) {
+        return res.status(500).json({
+            errCode: 1,
+            message: 'Missing inputs parameters',
+            users: []
+        })
+    }
+
     let users = await userService.getAllUsers(id)
     console.log(users)
     return res.status(200).json({
